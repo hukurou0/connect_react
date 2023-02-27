41c1a5cd3a2d2c6f3125e809c29074454419e449
hukurou0 <junjun.0712.on@gmail.com>
2023-02-27 19:41:29 +0900

    example axios

--- connect/src/components/App.tsx
+++ connect/src/components/App.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
+import axios from 'axios';
+
+axios.defaults.baseURL = "https://connectapi.herokuapp.com"
 
 function App() {
+
+  const [post, setPost] = React.useState({"status_code":1,
+                                          "data":{"id":[],"name":[]}});
+
+  function get(){
+    axios.get("/api/getDepartment")
+    .then(function (response) {
+    //受信したデータの処理
+    setPost(response.data);
+    })
+    .catch(function (error) {
+    //エラー処理
+    console.log(error);
+  });
+}
+
   return (
-    <div className="App">
-      this is component
+    <div>
+      <button onClick={get}>get</button>
+      <h1>{post.status_code}</h1>
+      <p>{post.data.id}</p>
+      <p>{post.data.name}</p>
     </div>
   );
 }
 
 export default App;
+
