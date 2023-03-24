a93cbd236c78a444abe191410e0e640c8175b4f3
taka-2120 <yu_tkhs@icloud.com>
2023-03-24 23:11:49 +0900

    feat: timetable

---
+++ src/common/UI/Components/ClassPicker.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import { createStyles, UnstyledButton, Menu, Group, rem } from '@mantine/core';
+import { useRecoilValue } from 'recoil';
+import { subjectsState } from '../../../Hooks/SubjectState';
+import { SubjectData } from '../../../Domain/Entities/SubjectEntity';
+
+const useStyles = createStyles((theme, { opened }: { opened: boolean }) => ({
+  control: {
+    width: rem(200),
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: `${theme.spacing.xs} ${theme.spacing.md}`,
+    borderRadius: theme.radius.md,
+    // border: `${rem(1)} solid ${theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[4]}`,
+    transition: 'background-color 150ms ease',
+    backgroundColor:
+      theme.colorScheme === 'dark' ? theme.colors.dark[opened ? 5 : 6] : opened ? theme.colors.gray[0] : theme.white,
+    // '&:hover': {
+    //   backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[0],
+    // },
+  },
+
+  label: {
+    fontWeight: 500,
+    fontSize: theme.fontSizes.sm,
+  },
+
+  icon: {
+    transition: 'transform 150ms ease',
+    transform: opened ? 'rotate(180deg)' : 'rotate(0deg)',
+  },
+}));
+
+interface Params {
+  selectedSubject: SubjectData | undefined;
+  setSelection: React.Dispatch<React.SetStateAction<SubjectData | undefined>>;
+}
+
+export const ClassPicker = ({ selectedSubject, setSelection }: Params) => {
+  const [opened, setOpened] = useState(false);
+  const { classes } = useStyles({ opened });
+  const subjects = useRecoilValue(subjectsState);
+  const items = subjects?.map((item) => (
+    <Menu.Item onClick={() => setSelection(item)} key={item.name}>
+      {item.name}
+    </Menu.Item>
+  ));
+
+  return (
+    <Menu onOpen={() => setOpened(true)} onClose={() => setOpened(false)} radius="md" width="target" withinPortal>
+      <Menu.Target>
+        <UnstyledButton className={classes.control}>
+          <Group spacing="xs">
+            <span className={classes.label}>{selectedSubject?.name ?? '空きコマ'}</span>
+          </Group>
+        </UnstyledButton>
+      </Menu.Target>
+      <Menu.Dropdown>{items}</Menu.Dropdown>
+    </Menu>
+  );
+};
