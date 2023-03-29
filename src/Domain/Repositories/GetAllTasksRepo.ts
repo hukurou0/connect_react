33bd84bb62f08cd5bf067e05b731fa7b18474789
taka-2120 <yu_tkhs@icloud.com>
2023-03-30 00:09:36 +0900

    refactor: handle error

--- src/Domain/Repositories/GetAllTasksRepo.ts
+++ src/Domain/Repositories/GetAllTasksRepo.ts
@@ -8,17 +8,34 @@ export const fetchAllTasks = async (): Promise<AllTasksResponse> => {
   try {
     const response = await axios.get(BASEURL + GET_TASKS);
     const { data } = response;
-    const allTasksResponse: AllTasksResponse = {
-      status_code: data.status_code,
-      data: {
-        display_ok: {
-          subject_taken: data.data.display_ok.subject_taken,
-          task_regist: data.data.display_ok.task_regist,
+    console.log(response);
+    
+    let allTasksResponse: AllTasksResponse;
+    try {
+      allTasksResponse = {
+        status_code: data.status_code,
+        data: {
+          display_ok: {
+            subject_taken: data.data.display_ok.subject_taken,
+            task_regist: data.data.display_ok.task_regist,
+          },
+          visible_limit: data.data.visible_limit,
+          tasks: data.data.tasks,
         },
-        visible_limit: data.data.visible_limit,
-        tasks: data.data.tasks,
-      },
-    };
+      };
+    } catch {
+      allTasksResponse = {
+        status_code: data.status_code,
+        data: {
+          display_ok: {
+            subject_taken: false,
+            task_regist: false,
+          },
+          visible_limit: "",
+          tasks: [],
+        },
+      };
+    }
     return allTasksResponse;
   } catch (error) {
     const errorData = makeErrorData(error);
